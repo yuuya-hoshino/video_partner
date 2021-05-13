a51524aa8adb24f1ff5cacdbb234eac7e6528e29
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2021-05-13 23:44:05 +0900

    edit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def top
+    @users = User.all.order('id DESC').limit(5)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,30 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all.order('id DESC').limit(5)
+    @user = User.find(current_user.id)
   end
 
   def show
+    @user = User.find_by(id: params[:id])
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.introduction = params[:introduction]
+    if @user.save
+      redirect_to(user_path(current_user.id))
+    else
+      render(edit_user_path(@user.id))
+    end
   end
 
   def logout
+    redirect_to("/")
   end
+
 end
