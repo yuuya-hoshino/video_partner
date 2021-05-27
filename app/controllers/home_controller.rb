acf64e818a9a832942efa40fc8112ef069ed532e
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2021-05-27 17:35:25 +0900

    追加機能

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def top
-    @users = User.all.order('id DESC').limit(5)
+    if user_signed_in?
+      @users = User.where.not(id: current_user.id).order('id DESC').limit(5)
+    else
+      @users = User.all.order('id DESC').limit(5)
+    end
   end
 end
