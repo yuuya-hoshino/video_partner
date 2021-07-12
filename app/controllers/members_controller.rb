eecf5c79d4445ea05ba7fd2daff4c2254f6e656f
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2021-07-12 14:40:30 +0900

    Update members_controller.rb

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,11 @@
 class MembersController < ApplicationController
+  include Pagy::Backend
   def index
-    @users = User.where.not(id: current_user.id)
+    @follower_count = Relationship.group(:follower_id).count(:following_id)
+    if params[:search]
+      @pagy,@users = pagy(User.where('name like ? OR introduction like ?', "%#{params[:search]}%","%#{params[:search]}%" ))
+    else
+      @pagy,@users = pagy(User.where.not(id: current_user.id).order('id DESC'))
+    end
   end
 end
