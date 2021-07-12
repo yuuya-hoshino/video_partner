1d3b6d7a7aff9f22de4849950a17a66fd2ca15b0
yuuya-hoshino <65810632+yuuya-hoshino@users.noreply.github.com>
2021-07-12 14:40:26 +0900

    Update home_controller.rb

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
   def top
     if user_signed_in?
       @users = User.where.not(id: current_user.id).order('id DESC').limit(5)
+      @follower_count = Relationship.where(follower_id: current_user.id).count
     else
       @users = User.all.order('id DESC').limit(5)
     end
